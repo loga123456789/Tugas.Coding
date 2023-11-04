f67269500052bab9f17c6ff913fda959233ab886
loga123456789 <147137907+loga123456789@users.noreply.github.com>
2023-11-04 13:20:29 +0700

    Add files via upload

---
+++ Tugas4.java
@@ -0,0 +1,12 @@
+public class Tugas4 {
+    public static void main(String[] args) {
+        // Mendeklarasikan array dengan tipe data integer
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // Menampilkan semua nilai dalam variabel array menggunakan perulangan for
+        System.out.println("Nilai dalam variabel array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
